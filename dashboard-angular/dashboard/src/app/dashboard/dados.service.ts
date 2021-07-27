f57ed16caf18efbcec530c9ceef653950313ae4b
phgusmao <pedro.gusmao1998@hotmail.com>
2021-07-27 14:06:07 -0300

    dashboard com gráficos

---
+++ dashboard-angular/dashboard/src/app/dashboard/dados.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { observable, Observable } from 'rxjs';
+
+// import { Observable } from 'rxjs/Observable';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DadosService {
+
+  readonly dados = [
+    ['Janeiro', 33],
+    ['Fevereiro', 68],
+    ['Março', 49],
+    ['Abril', 15],
+    ['Maio', 80],
+    ['Junho', 27],
+  ];
+
+  constructor() {}
+
+  obterDados(): Observable<any> {
+    return new Observable(observable => {
+      observable.next(this.dados);
+      observable.complete()
+    })
+  }
+}
